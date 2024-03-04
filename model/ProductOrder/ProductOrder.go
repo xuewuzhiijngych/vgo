eec377ab0f1a9c3db4bf31c9858eb1cae233386e
yanchunhao <601266867@qq.com>
2024-03-04 11:14:47 +0800

    2024/03/04 封装模型 Pagination

--- model/ProductOrder/ProductOrder.go
+++ model/ProductOrder/ProductOrder.go
@@ -1,12 +1,16 @@
 package ProductOrder
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"strconv"
 	"vgo/core/db"
 )
 
+// TableName 表名
 var TableName = "product_orders"
 
+// Build 数据模型
 type Build struct {
 	ID               uint    `gorm:"primary_key"  json:"id"`
 	OrderType        string  `gorm:"column:order_type" json:"order_type"`
@@ -21,6 +25,37 @@ type Build struct {
 	SuccessHandleNum int     `gorm:"column:success_handle_num" json:"success_handle_num"`
 }
 
+// PaginationResponse 分页响应
+type PaginationResponse struct {
+	Page     int     `json:"page"`
+	PageSize int     `json:"page_size"`
+	Total    int64   `json:"total"`
+	List     []Build `json:"list"`
+}
+
+// Query 查询
 func Query() (tx *gorm.DB) {
 	return db.GetCon().Table(TableName)
+}
+
+// Pagination 分页查询
+func Pagination(ctx *gin.Context) (response PaginationResponse) {
+	var list []Build
+	pageNo, _ := strconv.Atoi(ctx.DefaultQuery("page", "1"))
+	pageSizeNo, _ := strconv.Atoi(ctx.DefaultQuery("page_size", "10"))
+
+	// 总数
+	var total int64
+	Query().Count(&total)
+
+	// 分页查询
+	Query().Offset((pageNo - 1) * pageSizeNo).Limit(pageSizeNo).Find(&list)
+	// 响应
+	response = PaginationResponse{
+		Page:     pageNo,
+		PageSize: pageSizeNo,
+		Total:    total,
+		List:     list,
+	}
+	return response
 }
