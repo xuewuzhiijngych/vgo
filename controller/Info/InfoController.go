0f0f8ec1b2c2427aa898932e0d29b6a68aebb163
yanchunhao <601266867@qq.com>
2024-04-13 09:58:57 +0800

    2024/04/13 封装helper/model

--- controller/Info/InfoController.go
+++ controller/Info/InfoController.go
@@ -4,47 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 	"strconv"
 	"vgo/core/response"
+	"vgo/helper"
 	"vgo/model/Info"
 )
 
 // Index 列表
 func Index(ctx *gin.Context) {
-	res := Info.Pagination(ctx)
-
-	type InfoList struct {
-		Info.Build
-		Class string `json:"class"`
-	}
-
-	newList := make([]InfoList, len(res.List))
-	for i, v := range res.List {
-		newList[i].ID = v.ID             // 复制字段
-		newList[i].Name = "Mr." + v.Name // 修改字段
-		newList[i].Class = "数学"          // 新增字段
-	}
-
+	res := helper.Pagination(ctx, Info.TableName, Info.Build{})
 	response.Success(ctx, "成功", map[string]interface{}{
 		"page":     res.Page,
 		"pageSize": res.PageSize,
 		"total":    res.Total,
-		"lists":    newList,
+		"lists":    res.List,
 	}, nil)
 }
 
 // Detail 详情
 func Detail(ctx *gin.Context) {
-	query := Info.Detail(ctx)
-	if query.ID == 0 {
-		response.Fail(ctx, "数据不存在", nil, nil)
-		return
-	}
-	type InfoDetail struct {
-		Info.Build
-		Class string `json:"class"`
-	}
-	var detail InfoDetail
-	detail.Build = query
-	detail.Class = "Class-----"
+	detail := helper.First(ctx, Info.TableName, Info.Build{}, "id", "id,name,age")
 	response.Success(ctx, "成功", detail, nil)
 }
 
@@ -58,7 +35,7 @@ func Create(ctx *gin.Context) {
 		return
 	}
 
-	Info.Query().Create(&Info.Build{
+	helper.GETQuery(Info.TableName).Create(&Info.Build{
 		Name: name,
 		Age:  age,
 	})
---
+++ helper/model.go
@@ -0,0 +1,91 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"reflect"
+	"strconv"
+	"strings"
+	"vgo/core/db"
+)
+
+// BaseBuild 数据模型
+type BaseBuild interface {
+}
+
+// PaginationResponse 分页响应
+type PaginationResponse struct {
+	Page     int         `json:"page"`
+	PageSize int         `json:"page_size"`
+	Total    int64       `json:"total"`
+	List     interface{} `json:"list"`
+}
+
+// GETQuery 获取查询对象
+func GETQuery(TableName string) (tx *gorm.DB) {
+	return db.GetCon().Table(TableName)
+}
+
+// Pagination 分页查询
+func Pagination(ctx *gin.Context, TableName string, Build BaseBuild) (resp PaginationResponse) {
+	Query := db.GetCon().Table(TableName)
+	var list interface{}
+	var total int64
+	// 获取Build的实际类型
+	buildType := reflect.TypeOf(Build)
+	// 检查Build是否实现了某个接口或是个具体的结构体
+	switch reflect.TypeOf(Build).Kind() {
+	case reflect.Slice:
+		// 如果Build已经是切片，可以直接转换
+		list = reflect.New(reflect.SliceOf(buildType.Elem())).Interface()
+	case reflect.Struct:
+		// 如果Build是个结构体，创建一个该类型的切片
+		list = reflect.MakeSlice(reflect.SliceOf(buildType), 0, 0).Interface()
+	default:
+		panic("未知类型")
+	}
+	pageNo, _ := strconv.Atoi(ctx.DefaultQuery("page", "1"))
+	pageSizeNo, _ := strconv.Atoi(ctx.DefaultQuery("page_size", "10"))
+	// 总数
+	Query.Count(&total)
+	// 分页查询
+	Query.Order("id desc").Offset((pageNo - 1) * pageSizeNo).Limit(pageSizeNo).Find(&list)
+	// 响应
+	resp = PaginationResponse{
+		Page:     pageNo,
+		PageSize: pageSizeNo,
+		Total:    total,
+		List:     list,
+	}
+	return resp
+}
+
+// First 详情
+func First(ctx *gin.Context, TableName string, Build BaseBuild, options ...string) interface{} {
+	query := db.GetCon().Table(TableName)
+	var res interface{}
+	buildType := reflect.TypeOf(Build)
+	switch reflect.TypeOf(Build).Kind() {
+	case reflect.Slice:
+		res = reflect.New(reflect.SliceOf(buildType.Elem())).Interface()
+	case reflect.Struct:
+		res = reflect.MakeSlice(reflect.SliceOf(buildType), 0, 0).Interface()
+	default:
+		panic("未知类型")
+	}
+	queryField := "id"
+	selectFields := "*"
+	if len(options) >= 1 {
+		queryField = options[0]
+	}
+	if len(options) >= 2 {
+		selectFields = strings.Join(strings.Split(options[1], ","), ", ")
+	}
+	id := ctx.DefaultQuery(queryField, "0")
+	if selectFields != "*" {
+		query = query.Select(selectFields)
+	}
+	query.Where(fmt.Sprintf("%s = ?", queryField), id).First(&res)
+	return res
+}
