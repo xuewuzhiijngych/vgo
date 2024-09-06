a6c2c29330e09b47cd1171f51d13fc1e68eb2130
yanchunhao <601266867@qq.com>
2024-09-06 14:58:54 +0800

    2024/09/06 ws测试

--- app/Test/Test.go
+++ app/Test/Test.go
@@ -3,11 +3,9 @@ package Test
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/hibiken/asynq"
+	"github.com/gorilla/websocket"
+	"net/http"
 	"time"
-	"vgo/core/log"
-	"vgo/core/queue"
-	"vgo/job"
 )
 
 func Index(ctx *gin.Context) {
@@ -15,19 +13,66 @@ func Index(ctx *gin.Context) {
 	//if err != nil {
 	//	return
 	//}
-	client, err := queue.NewRedisClient()
+
+	// 获取WebSocket连接
+	wsUpgrader := websocket.Upgrader{
+		HandshakeTimeout: time.Second * 10,
+		ReadBufferSize:   1024,
+		WriteBufferSize:  1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	ws, err := wsUpgrader.Upgrade(ctx.Writer, ctx.Request, nil)
 	if err != nil {
-		fmt.Println("Failed to create redis client:", err)
+		fmt.Println(err)
 		return
 	}
-	defer queue.CloseRedisClient(client)
-	task, err := job.NewTestJob(42, "666666")
-	if err != nil {
-		log.GetLogger().Error(fmt.Sprintf("could not create task: %v", err))
+	defer func(ws *websocket.Conn) {
+		err := ws.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}(ws)
+
+	// 处理WebSocket消息
+	for {
+		messageType, p, err := ws.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		switch messageType {
+		case websocket.TextMessage:
+			fmt.Printf("处理文本消息, %s\n", string(p))
+			err := ws.WriteMessage(websocket.TextMessage, p)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		case websocket.BinaryMessage:
+			fmt.Println("处理二进制消息")
+		case websocket.CloseMessage:
+			fmt.Println("关闭websocket连接")
+			return
+		case websocket.PingMessage:
+			fmt.Println("处理ping消息")
+			err := ws.WriteMessage(websocket.PongMessage, []byte("ping"))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		case websocket.PongMessage:
+			fmt.Println("处理pong消息")
+			err := ws.WriteMessage(websocket.PongMessage, []byte("pong"))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		default:
+			fmt.Printf("未知消息类型: %d\n", messageType)
+			return
+		}
 	}
-	info, err := client.Enqueue(task, asynq.ProcessIn(time.Second*3))
-	if err != nil {
-		log.GetLogger().Error(fmt.Sprintf("could not enqueue task: %v", err))
-	}
-	log.GetLogger().Info(fmt.Sprintf("enqueued task: id=%s queue=%s", info.ID, info.Queue))
 }
--- go.mod
+++ go.mod
@@ -43,6 +43,7 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
